a7d71f38eb17d6fece13421c64cfaf5c5d856b9c
Renato Cesar <62253156+RenatoCesarF@users.noreply.github.com>
2021-06-02 10:27:30 -0400

    :fire: Remove test code

--- source/world.cs
+++ source/world.cs
@@ -15,20 +15,18 @@ namespace Mars{
 
             line = new Line2D( new LineSegment(Vector2.Zero, new Vector2(500,500)));
             box = new Box2D(new Vector2(100,200),new Vector2(500,200));
-
-            
         }
         public virtual void Update(){
             line.Update();
             line.lineSegment.to = Global.mouseControl.getMousePosition();
                
             box.getRigidbody2D().setRotation(rotate);
-
+            rotate += 0.01f;
             if(Global.keyboard.GetPress("Space")){
                 line.lineSegment.from = Global.mouseControl.getMousePosition();
             }
 
-            if(intersectionDetector2D.pointInBox2D(Global.mouseControl.getMousePosition(),box)){
+            if(intersectionDetector2D.lineInBox2D(line.lineSegment,box)){
                 Global.console.print("FUNFA");
                 return;
             }
@@ -37,19 +35,9 @@ namespace Mars{
 
         }
         public virtual void Draw(Vector2 OFFSET){
-           
             box.Draw();
-            
-            // laaa.Draw(OFFSET);
-        
+
             line.Draw(OFFSET);
-
-            LineSegment a = new LineSegment( line.lineSegment.from, line.lineSegment.to);  
-            a.from =  MarsMath.rotate(a.getStart(),box.getRigidbody2D().getRotation(),box.getRigidbody2D().getPosition());
-            a.to = MarsMath.rotate(a.getEnd(),box.getRigidbody2D().getRotation(),box.getRigidbody2D().getPosition());
-            Line2D dd = new Line2D(a,8);
-
-            dd.Draw(OFFSET);
         }
     }
 }
